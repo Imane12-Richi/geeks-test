98b74364344a7dae3bcbd5193e62393abaaa2eef
Imane12-Richi <imanerichi8@gmail.com>
2026-01-28 09:12:36 +0100

    done

--- week_1/day_1/exercises/exercises_xp.js
+++ week_1/day_1/exercises/exercises_xp.js
@@ -1,9 +1,146 @@
-// ===== Exercise 1
+//===== Exercise 1
+const people = ["Greg", "Mary", "Devon", "James"];//1
+people.splice(0,1);
+console.log(people)
 
-// ===== Exercise 2
 
-// ===== Exercise 3
+people[3]="jason";               
+console.log(people)
 
-// ===== Exercise 4
+  const people = ["Greg", "Mary", "Devon", "James"];//3
+ people.push("imane")
+console.log(people)
 
-// ===== Exercise ...
+ const people = ["Greg", "Mary", "Devon", "James"];//4
+people.indexOf("Mary")
+console.log(people.indexOf("Mary"))
+
+ const people = ["Mary","Devon","Jason","Yourname"];//5
+const array=people.slice(1,3);
+console.log(array)
+
+ const people = ["Mary","Devon","Jason","Yourname"];//6
+const index = people.indexOf("Foo");
+console.log(index)
+
+ const people = ["Mary","Devon","Jason","Yourname"];//7
+const last = people[people.length - 1]; // is not in the array
+console.log(last)
+
+//loops
+ const people = ["Mary", "Devon", "Jason", "Yourname"];//1
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i]);
+}
+//===== Exercise 2
+
+const people = ["Mary", "Devon", "Jason", "Yourname"];
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i]);  // afficher la personne
+  if (people[i] === "Devon") {
+    break;                 // sortir de la boucle
+  }
+}
+
+ const colors = ["Black", "pink", "green", "reed","bleu"];//2
+for (let i = 0; i < colors.length; i++) {
+  console.log(`My #${i + 1} choice is ${colors[i]}`);
+}
+  
+ suffixes = ["st", "nd", "rd", "th", "th"];
+for (let i = 0; i < colors.length; i++) {
+  console.log(`My ${i + 1}${suffixes[i]} choice is ${colors[i]}`);
+}
+//===== Exercise 3
+
+ let number = prompt("Enter a number:");//1
+console.log(typeof number);
+
+  number = Number(number);//2
+while (number < 10) {
+  number = Number(prompt("Enter a new number:"));
+}
+//===== Exercise 4
+const building = {
+  numberOfFloors: 4,
+  numberOfAptByFloor: {
+    firstFloor: 3,
+    secondFloor: 4,
+    thirdFloor: 9,
+    fourthFloor: 2,
+  },
+  nameOfTenants: ["Sarah", "Dan", "David"],
+  numberOfRoomsAndRent: {
+    sarah: [3, 990],
+    dan: [4, 1000],
+    david: [1, 500],
+  },
+};
+
+ console.log(building.numberOfFloors);//1
+
+ console.log(  //2
+  building.numberOfAptByFloor.firstFloor +
+  building.numberOfAptByFloor.thirdFloor
+);
+  console.log(        //3
+  building.nameOfTenants[1],
+  building.numberOfRoomsAndRent.dan[0]
+);
+
+if (
+  building.numberOfRoomsAndRent.sarah[1] +
+  building.numberOfRoomsAndRent.david[1] >
+  building.numberOfRoomsAndRent.dan[1]
+) {
+  building.numberOfRoomsAndRent.dan[1] = 1200;
+}
+//exercice 6
+const details = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'reindeer'
+};
+
+let sentence = "";
+
+for (let key in details) {
+  sentence += key + " " + details[key] + " ";
+}
+
+console.log(sentence.trim());
+
+//exercice 5
+const family = {  //1
+  father: "Ahmed",
+  mother: "Fatima",
+  daughter: "Ima",
+  son: "Youssef"
+};
+
+for (let key in family) {  //2
+  console.log(key);
+}
+
+for (let key in family) {  //3  
+  console.log(family[key]);
+}
+//exercice 7
+const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+
+let secretName = [];
+
+for (let i = 0; i < names.length; i++) {
+  secretName.push(names[i][0]);
+}
+
+secretName.sort();
+
+console.log(secretName.join(""));
+
+
+
+
+
+
+
--- week_1/day_1/exercises/exercises_xp_ninja.js
+++ week_1/day_1/exercises/exercises_xp_ninja.js
@@ -1,6 +1,59 @@
 // ===== Exercise 1
+const person1 = {  //2
+  fullName: "Sarah Smith",
+  mass: 68,
+  height: 1.65,
+  bmi: function () {
+    return this.mass / (this.height * this.height);
+  }
+};
+
+const person2 = {
+  fullName: "John Doe",
+  mass: 85,
+  height: 1.8,
+  bmi: function () {
+    return this.mass / (this.height * this.height);
+  }
+};
+
+function compareBMI(p1, p2) {  //2
+  if (p1.bmi() > p2.bmi()) {
+    console.log(p1.fullName + " has the larger BMI");
+  } else if (p2.bmi() > p1.bmi()) {
+    console.log(p2.fullName + " has the larger BMI");
+  } else {
+    console.log("Both have the same BMI");
+  }
+}
+compareBMI(person1, person2); //3
+
 
 // ===== Exercise 2
+function findAvg(gradesList) {
+  let sum = 0;
+
+  // Calculer la somme des notes 
+  for (let i = 0; i < gradesList.length; i++) {
+    sum += gradesList[i];
+  }
+
+  // Calculer la moyenne
+  let average = sum / gradesList.length;
+
+  // Afficher la moyenne
+  console.log("Average:", average);
+
+  // Vérifier si la moyenne est supérieure à 65
+  if (average > 65) {
+    console.log("You passed!");
+  } else {
+    console.log("You did not pass.");
+  }
+}
+
+const myGrades = [70, 80, 65, 90, 50];
+findAvg(myGrades);
 
 // ===== Exercise 3
 
